f3aa31084c68849517c1d61253139af480910ec1
Kazuma.I <kazuma.work77@gmail.com>
2023-07-29 13:36:57 +0900

    Fix: 修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,52 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  connectorsForWallets,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { createConfig, WagmiConfig, configureChains } from "wagmi";
+import { mainnet, polygon } from "wagmi/chains";
+import {
+  walletConnectWallet,
+  rainbowWallet,
+  metaMaskWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { rainbowWeb3AuthConnector } from "@/libs/RainbowWeb3authConnector";
+import { AppProps } from "next/app";
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon],
+  [
+    alchemyProvider({ apiKey: "7wSu45FYTMHUO4HJkHjQwX4HFkb7k9Ui" }),
+    alchemyProvider({ apiKey: "fGXusgBUDC-OPy6XI8IFRvu1i7sbWsYj" }),
+    publicProvider(),
+  ]
+);
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      rainbowWallet({ chains }),
+      walletConnectWallet({ chains }),
+      metaMaskWallet({ chains }),
+      rainbowWeb3AuthConnector({ chains }) as any,
+    ],
+  },
+]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
